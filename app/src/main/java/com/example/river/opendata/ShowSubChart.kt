9edd15e1f6cac805d07a70b08ec33b16c4b50c85
Aria <angel2248664@hotmail.com>
2018-10-12 23:31:22 +0800

    A,D sample Aria

--- app/src/main/java/com/example/river/opendata/ShowSubChart.kt
+++ app/src/main/java/com/example/river/opendata/ShowSubChart.kt
@@ -2,10 +2,38 @@ package com.example.river.opendata
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.sub_chart.*
 
 class ShowSubChart: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sub_chart)
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        spinnerInit()
+
     }
+
+    fun spinnerInit(){
+        val yearList = ArrayAdapter.createFromResource(this,  R.array.year_array, R.layout.spinner_center_item)
+        val monthList = ArrayAdapter.createFromResource(this, R.array.month_array, R.layout.spinner_center_item)
+
+        year_spinner.adapter = yearList
+        month_spinner.adapter = monthList
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item!!.itemId){
+            R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
 }
